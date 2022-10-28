7019d12dda93a796d90bba515ac37ffe1d6f5b8b
NarvaezE <enarvaez_19@alu.uabcs.mx>
2022-10-27 19:16:24 -0500

    UserSeeder y ClientSeeder

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        DB::table('client')->insert([
+            'name' => 'Edgar',
+            'lastname' => 'Narvaez',
+            'email' => 'enarvaez_19@alu.uabcs.mx',
+            'phone_number' => '6121118415'
+        ]);
+        
+        DB::table('client')->insert([
+            'name' => 'Isaac',
+            'lastname' => 'Parkour',
+            'email' => 'isaacp@alu.uabcs.mx',
+            'phone_number' => '6123447890'
+        ]);
+    
+    }
+}
